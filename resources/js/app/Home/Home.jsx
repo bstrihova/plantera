7986d9ef58ec22a0db3820f07d09ca9f0d7b61a5
bstrihova <bara@MacBook-Pro.local>
2020-11-13 16:50:49 +0100

    strcutrued the scss, updated git ignore hoping it wont cause problems anymore

--- resources/js/app/Home/Home.jsx
+++ resources/js/app/Home/Home.jsx
@@ -25,19 +25,6 @@ function Home() {
 
                 <section className="content">
 
-                    {/* <GridList className={classes.gridList} cols={2} spacing={12}>
-                        <GridListTile>
-                        <img src="https://images.unsplash.com/photo-1517848568502-d03fa74e1964?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="monstera"/>
-                        </GridListTile>
-                        <GridListTile>
-                        <img src="https://images.unsplash.com/photo-1591958911259-bee2173bdccc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt="pothos lime"/>
-                        </GridListTile>
-                        <GridListTile>
-                        <img src="https://images.unsplash.com/photo-1602491674275-316d95560fb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" alt="syngonium"/>
-                        </GridListTile>
-                    
-                    </GridList> */}
-
                     <PostGrid/>
 
 
