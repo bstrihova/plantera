cd7892d4bf07d2462410abed4861b75124b5b2eb
SteTheDiver <ste@name.com>
2020-11-16 14:53:22 +0100

    set up Post create and edit

--- resources/js/app/Post/PostCreate.jsx
+++ resources/js/app/Post/PostCreate.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
-import ExampleComponent from "../ExampleComponent/ExampleComponent";
 import Typography from "@material-ui/core/Typography";
-import PostGrid from "../components/PostGrid/PostGrid";
-import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import MuiSelect from "@material-ui/core/Select";
+import MuiMenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import { Link } from "react-router-dom";
 
 function PostCreate() {
     return (
@@ -14,31 +17,75 @@ function PostCreate() {
                     Post new plant
                 </Typography>
             </Box>
-            <Box>
-                <figure className="myPictures">
-                    <img
-                        src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
-                        alt=""
+            <Box className="main__container__shadow">
+                <div className="main__container">
+                    <figure className="myPictures">
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
+                            alt=""
+                        />
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
+                            alt=""
+                        />
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
+                            alt=""
+                        />
+                    </figure>
+                    <Button
+                        className="button"
+                        alignSelf="center"
+                        color="primary"
+                        variant="contained"
+                        size="large"
+                    >
+                        + Add plant picture
+                    </Button>
+                    <TextField
+                        color="primary"
+                        label="Name of the plant"
+                        placeholder="Add the name of your plant"
                     />
-                    <img
-                        src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
-                        alt=""
-                    />
-                    <img
-                        src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
-                        alt=""
-                    />
-                </figure>
+                    <Box display="flex" justifyContent="space-around">
+                        <FormControl>
+                            <InputLabel id="status">Selling?</InputLabel>
+                            <MuiSelect labelId="status">
+                                <MuiMenuItem value="1">Donate</MuiMenuItem>
+                                <MuiMenuItem value="2">Sell</MuiMenuItem>
+                                <MuiMenuItem value="2">Swap</MuiMenuItem>
+                            </MuiSelect>
+                        </FormControl>
+                        <TextField
+                            color="primary"
+                            label="Price"
+                            placeholder="Add your price"
+                        />
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <TextField
+                            id="filled-multiline-static"
+                            label="Description"
+                            multiline
+                            rows={4}
+                            placeholder="Add your description"
+                            variant="filled"
+                        />
+                    </Box>
 
-                <Button
-                    className="button"
-                    alignSelf="center"
-                    color="primary"
-                    variant="contained"
-                    size="large"
-                >
-                    + Post new picture
-                </Button>
+                    {/* <Link to="/messages/create">  */}
+                    <Button
+                        className="button--post"
+                        color="primary"
+                        variant="contained"
+                        size="medium"
+                        disableRipple
+                        style={{ textTransform: "none" }}
+                    >
+                        Post new plant
+                    </Button>
+                    {/* </Link> */}
+                </div>
             </Box>
         </div>
     );
--- resources/js/app/Post/PostEdit.jsx
+++ resources/js/app/Post/PostEdit.jsx
@@ -1,11 +1,101 @@
-import React from 'react'
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import MuiSelect from "@material-ui/core/Select";
+import MuiMenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import { Link } from "react-router-dom";
 
 function PostEdit() {
     return (
         <div>
-            post edit
+            <Box textAlign="center" mt={4}>
+                <Typography variant="h1" color="primary" fontSize="bold">
+                    Edit your post
+                </Typography>
+            </Box>
+            <Box className="main__container__shadow">
+                <div className="main__container">
+                    <figure className="myPictures">
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
+                            alt=""
+                        />
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
+                            alt=""
+                        />
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0260/3037/4957/products/medium-plant-snake-white-pot_720x.jpg?v=1597702214"
+                            alt=""
+                        />
+                    </figure>
+                    <Button
+                        className="button"
+                        alignSelf="center"
+                        color="primary"
+                        variant="contained"
+                        size="large"
+                    >
+                        + Add plant picture
+                    </Button>
+                    <TextField
+                        color="primary"
+                        label="Name of the plant"
+                        placeholder="Add the name of your plant"
+                    />
+                    <Box display="flex" justifyContent="space-around">
+                        <FormControl>
+                            <InputLabel id="status">Selling?</InputLabel>
+                            <MuiSelect labelId="status">
+                                <MuiMenuItem value="1">Donate</MuiMenuItem>
+                                <MuiMenuItem value="2">Sell</MuiMenuItem>
+                                <MuiMenuItem value="2">Swap</MuiMenuItem>
+                            </MuiSelect>
+                        </FormControl>
+                        <TextField
+                            color="primary"
+                            label="Price"
+                            placeholder="Add your price"
+                        />
+                    </Box>
+                    <FormControl>
+                        <InputLabel id="status">Status</InputLabel>
+                        <MuiSelect labelId="status">
+                            <MuiMenuItem value="1">Available</MuiMenuItem>
+                            <MuiMenuItem value="2">Sold</MuiMenuItem>
+                        </MuiSelect>
+                    </FormControl>
+                    <Box display="flex" justifyContent="center">
+                        <TextField
+                            id="filled-multiline-static"
+                            label="Description"
+                            multiline
+                            rows={4}
+                            placeholder="Add your description"
+                            variant="filled"
+                        />
+                    </Box>
+
+                    {/* <Link to="/messages/create">  */}
+                    <Button
+                        className="button--post"
+                        color="primary"
+                        variant="contained"
+                        size="small"
+                        disableRipple
+                        style={{ textTransform: "none" }}
+                    >
+                        Confirm changes
+                    </Button>
+                    {/* </Link> */}
+                </div>
+            </Box>
         </div>
-    )
+    );
 }
 
-export default PostEdit
+export default PostEdit;
